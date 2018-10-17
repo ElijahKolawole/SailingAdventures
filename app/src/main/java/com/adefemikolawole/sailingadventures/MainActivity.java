6d24015fb36fa2dc93044fce7a1ff11c9c9bf03e
Adefemi Kolawole <akolawole@ggc.edu>
2018-10-16 20:30:18 -0400

    porject 90% done

--- app/src/main/java/com/adefemikolawole/sailingadventures/MainActivity.java
+++ app/src/main/java/com/adefemikolawole/sailingadventures/MainActivity.java
@@ -13,6 +13,9 @@ import android.widget.TextView;
 import java.text.DateFormat;
 import java.util.Calendar;
 
+import static com.adefemikolawole.sailingadventures.R.id.*;
+import static com.adefemikolawole.sailingadventures.R.menu.menu_main;
+
 public class MainActivity extends AppCompatActivity {
     private TextView reservation;
 
@@ -20,11 +23,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        reservation = findViewById(R.id.txtReservation);
+        reservation = findViewById(txtReservation);
         Button button = findViewById(R.id.btnDate);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                new DatePickerDialog(MainActivity.this, d, c.get(Calendar.YEAR), c.get(Calendar.MONTH), c.get(Calendar.DAY_OF_MONTH)).show();
             }
         });
     }
@@ -42,11 +46,16 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        return super.onCreateOptionsMenu(menu);
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+
+
         return super.onOptionsItemSelected(item);
     }
 }
